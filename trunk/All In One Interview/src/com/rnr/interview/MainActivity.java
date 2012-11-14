e65c69d381e2274ffddc20df8776a8583fde9c98
ravindrakamble4u@gmail.com <ravindrakamble4u@gmail.com@ee3a30fa-681d-f170-4507-490056233ef9>
2012-11-14 17:04:58 +0000

    Added new Project

---
+++ trunk/All In One Interview/src/com/rnr/interview/MainActivity.java
@@ -0,0 +1,20 @@
+package com.rnr.interview;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
